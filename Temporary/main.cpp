43934781580d43ef992f1f999f2fa931d4dc51db
Ihor Holovko <ihorholovko@JackydeMacBook-Pro.local>
2018-10-31 22:18:19 +0800

    Initial Commit

---
+++ Temporary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Temporary
+//
+//  Created by Ihor Holovko on 2018/10/31.
+//  Copyright © 2018 Ihor Holovko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
